1434bc0ee2a7d2985fd3f73b78b16966eca76ea5
Mohammed <mohammedelbanawey264@gmail.com>
2025-10-26 18:37:47 +0300

    handle logging and signup errors

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,30 +2,37 @@ import axios from 'axios'
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthProvider'
+import toast from 'react-hot-toast'
 
 const Login = () => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
+  const [errors, setErrors] = useState({})
   const { user, login } = useAuth()
   const navigate = useNavigate()
   async function handleLogin(e) {
     e.preventDefault();
-    const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, {
-      email,
-      password
-    }, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      withCredentials: true
-    });
-    login(data.user)
-    if (data?.user.role === "MERCHANT") {
-      return navigate('/dashboard')
-    } else {
-      navigate('/')
+    try {
+      const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, {
+        email,
+        password
+      }, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true
+      });
+      login(data.user)
+      if (data?.user.role === "MERCHANT") {
+        return navigate('/dashboard')
+      } else {
+        navigate('/')
+      }
+    } catch (error) {
+      const errors = error.response.data.errors
+
+      setErrors(errors)
     }
   }
 
@@ -36,11 +43,12 @@ const Login = () => {
         <div className='flex flex-col gap-2'>
           <label htmlFor="" className='font-semibold text-lg'>Email</label>
           <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className='p-4 border-2 border-slate-500 focus:ring-4 transition duration-300 ring-slate-700 rounded-xl ' placeholder='Enter Your Email' />
+          <p className='text-red-500'>{errors.email}</p>
         </div>
         <div className='flex flex-col gap-2'>
           <label htmlFor="" className='font-semibold text-lg'>Password</label>
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className='p-4 border-2 border-slate-500 focus:ring-4 transition duration-300 ring-slate-700 rounded-xl ' placeholder='Enter Your Password' />
-
+          <p className='text-red-500'>{errors?.password}</p>
         </div>
         <p>Don't have an account? <Link to={'/signup'} className='text-blue-500 underline'>SignUp</Link></p>
         <button className='bg-black rounded-lg text-white cursor-pointer py-2'>Login</button>
